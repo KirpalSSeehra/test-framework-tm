3f05296af28e8ab609244d70cfb536bf73c9cc48
Kirpal Seehra <kirpal.seehra@tesco.com>
2023-03-20 19:35:18 +0000

    updated conftest file to be able to test on different browsers and urls

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,17 +1,37 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 
 # Launching browser and opening website
 @pytest.fixture(autouse=True)
-def setup(request):
-    driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    driver.get("https://www.tescomobile.com/")
+def setup(request, browser, url):
+    if browser == "chrome":
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    elif browser == "firefox":
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser == "edge":
+        driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    driver.get(url)
     driver.maximize_window()
     driver.execute_cdp_cmd('Network.setBlockedURLs', {"urls": ["tms.tescomobile.com"]})
     driver.execute_cdp_cmd('Network.enable', {})
     request.cls.driver = driver
     yield
     driver.close()
+
+def pytest_addoption(parser):
+    parser.addoption("--browser")
+    parser.addoption("--url")
+
+@pytest.fixture(scope="class", autouse=True)
+def browser(request):
+        return request.config.getoption("--browser")
+
+@pytest.fixture(scope="class", autouse=True)
+def url(request):
+        return request.config.getoption("--url")
 
 
